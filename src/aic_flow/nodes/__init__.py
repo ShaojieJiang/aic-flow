d5e43de74034f4047148b988fd2a5d6f8cdbc55a
Shaojie Jiang <shaojie.jiang1@gmail.com>
2025-05-09 08:48:01 +0200

    Update examples

--- src/aic_flow/nodes/__init__.py
+++ src/aic_flow/nodes/__init__.py
@@ -5,6 +5,7 @@ from aic_flow.nodes.code import PythonCodeNode
 from aic_flow.nodes.registry import NodeMetadata, NodeRegistry, registry
 from aic_flow.nodes.telegram import TelegramNode
 
+
 __all__ = [
     "BaseNode",
     "NodeMetadata",
